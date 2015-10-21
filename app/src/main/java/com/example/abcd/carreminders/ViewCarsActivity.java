9ccd390f1ff376a912a27acd9213438c972e1d77
Marco <marcoblagoiev@yahoo.com>
2015-10-21 13:06:03 +0300

    Fixed the licence and brand validation

---
+++ app/src/main/java/com/example/abcd/carreminders/ViewCarsActivity.java
@@ -0,0 +1,13 @@
+package com.example.abcd.carreminders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewCarsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_cars);
+    }
+}
