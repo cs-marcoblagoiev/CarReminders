bea9a201652b0133e4f5a522e807b6811cb5b690
Marco <marcoblagoiev@yahoo.com>
2015-10-22 22:07:08 +0300

    Improved the layout for single car

--- app/src/main/java/com/example/abcd/carreminders/ViewCarsActivity.java
+++ app/src/main/java/com/example/abcd/carreminders/ViewCarsActivity.java
@@ -26,20 +26,6 @@ public class ViewCarsActivity extends BaseActivity {
         setContentView(R.layout.activity_view_cars);
         super.onCreateDrawer(savedInstanceState);
 
-        // drop this database if already exists
-        //db.onUpgrade(db.getWritableDatabase(), 1, 2);
-
-
-/*        Car car = new Car();
-        car.setLicence("Car1");
-        car.setBrand("Brand1");
-        db.createCar(car);
-
-        Car car2 = new Car();
-        car2.setLicence("Car2");
-        car2.setBrand("Brand2");
-        db.createCar(car);*/
-
         // get all the cars
         list = db.getAllCars();
         List listCars = new ArrayList();
@@ -50,19 +36,11 @@ public class ViewCarsActivity extends BaseActivity {
             listCars.add(i, list.get(i));
         }
 
-
-
-        //myAdapter = new ArrayAdapter(this, R.layout.row_layout, R.id.listText, listTitle);
-
        ListView m_listview = (ListView) findViewById(R.id.list);
-/*         Log.d("DEBUG", car.getBrand());
-        Log.d("DEBUG",car.getLicence());
-        String[] items = new String[] {car.getBrand(), car.getLicence(), car2.getLicence(), car2.getBrand()};*/
-        //car.getBrand(), car.getLicence(), car2.getBrand(), car2.getBrand()
 
         ArrayAdapter<String> adapter =
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, android.R.id.text1, listCars){
-                    //overridning the method so we can use both text items
+                    //overriding the method so we can use both text items
                     @Override
                     public View getView(int position, View convertView, ViewGroup parent) {
                         View view = super.getView(position, convertView, parent);
--- app/src/main/java/com/example/abcd/carreminders/ViewSingleCarActivity.java
+++ app/src/main/java/com/example/abcd/carreminders/ViewSingleCarActivity.java
@@ -1,7 +1,16 @@
 package com.example.abcd.carreminders;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewSingleCarActivity extends BaseActivity {
     JCGSQLiteHelper db = new JCGSQLiteHelper(this);
@@ -11,10 +20,61 @@ public class ViewSingleCarActivity extends BaseActivity {
         setContentView(R.layout.activity_view_single_car);
         super.onCreateDrawer(savedInstanceState);
 
+        final List listFields = new ArrayList();
+
+        listFields.add(0,"Licence Plate");
+        listFields.add(1,"Brand, model");
+        listFields.add(2,"Usage");
+        listFields.add(3,"Insurance");
+        listFields.add(4,"Vehicle Inspection");
+        listFields.add(5,"Road Tax");
+        listFields.add(6,"Fire Extinguisher");
+        listFields.add(7,"Medical Kit");
+        listFields.add(8,"Rate");
+
         Car car=db.findCar(1);
 
-        TextView licenceText = (TextView) findViewById(R.id.licencePlateAdd);
-        licenceText.append(car.getLicence());
+/*        TextView licenceText = (TextView) findViewById(R.id.licencePlateAdd);
+        licenceText.append(car.getLicence());*/
+
+        //List listCars = new ArrayList();
+
+
+
+/*
+        for (int i = 0; i < list.size(); i++) {
+            listCars.add(i, list.get(i));
+        }
+*/
+
+        ListView m_listview = (ListView) findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, android.R.id.text1, listFields){
+                    //overriding the method so we can use both text items
+                    @Override
+                    public View getView(int position, View convertView, ViewGroup parent) {
+                        View view = super.getView(position, convertView, parent);
+                        TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                        TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+                        text1.setText(listFields.get(position).toString());
+                        text2.setText(listFields.get(position).toString());
+                        return view;
+                    }
+                };
+
+        m_listview.setAdapter(adapter);
+
+        m_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), ViewSingleCarActivity.class);
+                //intent.putExtra("id", )
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
