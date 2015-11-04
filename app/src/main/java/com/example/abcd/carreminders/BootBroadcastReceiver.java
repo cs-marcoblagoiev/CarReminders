862bfa1fd58e12d32dcfd1be627c2676bef360ef
Marco <marcoblagoiev@yahoo.com>
2015-11-04 13:17:22 +0200

    Alarms still dont work on restart

--- app/src/main/java/com/example/abcd/carreminders/BootBroadcastReceiver.java
+++ app/src/main/java/com/example/abcd/carreminders/BootBroadcastReceiver.java
@@ -3,12 +3,14 @@ package com.example.abcd.carreminders;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 
 //needed so we can restart the alarms after phone reboot
 public class BootBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context pContext, Intent intent) {
+        Log.d("DebugAlarm", "ON BOOTBraodcastReceiver");
         // Do your work related to alarm manager
         ManageAlarms ma = new ManageAlarms();
         ma.setAlarmForAllCars(pContext);
