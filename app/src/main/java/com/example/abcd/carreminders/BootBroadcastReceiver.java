f9f1ad783044372b757dba927579cb6a3d3cb69e
Mihai Iliescu <Mihai Iliescu>
2015-11-05 09:58:28 +0100

    Small changes

--- app/src/main/java/com/example/abcd/carreminders/BootBroadcastReceiver.java
+++ app/src/main/java/com/example/abcd/carreminders/BootBroadcastReceiver.java
@@ -4,12 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 
 //needed so we can restart the alarms after phone reboot
 public class BootBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context pContext, Intent intent) {
+        Toast.makeText(pContext, "In BootBroadcastReceiver", Toast.LENGTH_LONG);
         Log.d("DebugAlarm", "ON BOOTBraodcastReceiver");
         // Do your work related to alarm manager
         ManageAlarms ma = new ManageAlarms();
